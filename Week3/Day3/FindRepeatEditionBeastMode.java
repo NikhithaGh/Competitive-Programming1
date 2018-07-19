a4e42875b2024981b292b5b78dc68ab21f45cfa2
NikhithaGh <40629077+NikhithaGh@users.noreply.github.com>
2018-07-19 12:01:19 +0530

    Update FindRepeatEditionBeastMode.java

--- Week3/Day3/FindRepeatEditionBeastMode.java
+++ Week3/Day3/FindRepeatEditionBeastMode.java
@@ -1,1 +1,85 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
+import static org.junit.Assert.*;
+import java.util.*;
+
+public class FindRepeatEditionBeastMode {
+
+    public static int findDuplicate(int[] arr) {
+
+        // find a number that appears more than once ... in O(n) time
+        for(int i=0;i<arr.length;i++){
+            int x=Math.abs(arr[i]);
+            if(arr[x]>0) arr[x]=-arr[x];
+            else{
+                return Math.abs(arr[i]);
+            }
+        }
+
+        return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // tests
+
+    @Test
+    public void justTheRepeatedNumberTest() {
+        final int[] numbers = {1, 1};
+        final int expected = 1;
+        final int actual = findDuplicate(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shortArrayTest() {
+        final int[] numbers = {1, 2, 3, 2};
+        final int expected = 2;
+        final int actual = findDuplicate(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mediumArrayTest() {
+        final int[] numbers = {1, 2, 5, 5, 5, 5};
+        final int expected = 5;
+        final int actual = findDuplicate(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void longArrayTest() {
+        final int[] numbers = {4, 1, 4, 8, 3, 2, 7, 6, 5};
+        final int expected = 4;
+        final int actual = findDuplicate(numbers);
+        assertEquals(expected, actual);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
